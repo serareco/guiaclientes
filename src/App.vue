635bca38caf58b872b08aa729971410b1e9d9fe8
serareco <sergiovareco@icloud.com>
2021-04-19 19:58:21 -0300

    creacion de las partes de la app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="container"  id="app">
-    <div class="row">
-      <Task/>
+  <div id="app">
+    <Nav />
+    <div class="container-fluid my-md-4">
+      <aside></aside>
+      <main>
+        <div id="content">          
+          <div class="row" >
+            <div class="col-md-4 col-sm-4" v-for="task in tasks" :key="task.id">
+              <Task :datos="task" />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Task from './components/Task'
+import Nav from "./components/Nav";
+import Task from "./components/Task";
+import Footer from "./components/Footer";
+
 export default {
-  name: 'App',components:{
-    Task
-  }
-}
+  name: "App",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          description: "comprar pan",
+          responsable: "Sergio",
+          detail: "Ir al kiosco de enfrente a comprar pan",
+        },
+        {
+          id: 2,
+          description: "comprar leche",
+          responsable: "Yanet",
+          detail: "Ir al kiosco de enfrente a comprar leche",
+        },
+        {
+          id: 3,
+          description: "pasear el perro",
+          responsable: "Sergio",
+          detail: "sacar el perro a pasear.",
+        },
+      ],
+    };
+  },
+  components: {
+    Nav,
+    Task,
+    Footer,
+  },
+};
 </script>
 
 <style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,15 +1,26 @@
 <template>
-    <div class="col">
-        <button type="button" class="btn btn-success">Boton de prueba!</button>
-    </div> 
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ datos.description }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">{{ datos.responsable }}</h6>
+      <p class="card-text">{{ datos.detail }}</p>
+      <a href="#" class="card-link">Resuelto</a>
+      <a href="#" class="card-link">Postponer</a>
+      <a href="#" class="card-link">Rechazar</a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {};
+  },
+  props: {
+    datos: Object,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
